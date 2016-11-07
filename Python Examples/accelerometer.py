ed4ace0be6cf2b45a55218bc224fabb8efcb107c
Carol Willing <carolcode@willingconsulting.com>
2016-11-06 21:40:08 -0800

    Refresh the accelerometer example

--- Python Examples/accelerometer.py
+++ Python Examples/accelerometer.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python
-import time
-import sys
 
-# Import CircuitPlayground class from the circuitplayground.py in the same directory.
+"""
+accelerometer.py
+----------------
+
+A basic accelerometer example
+
+
+Example
+-------
+
+python accelerometer.py /dev/ttyACM0
+
+"""
+
+import sys
+import time
+
+# Import CircuitPlayground class from `circuitplayground.py` in the same directory.
 from circuitplayground import CircuitPlayground
 
 
-# Grab the serial port from the command line parameters.
+# Check if command specifies a serial port
 if len(sys.argv) != 2:
-    print('ERROR! Must specify the serial port as command line parameter.')
+    print('ERROR: Must specify the serial port as command line parameter.')
     sys.exit(-1)
 port = sys.argv[1]
 
-# Connect to Circuit Playground board on specified port.
+# Connect to Circuit Playground board on specified serial port.
 board = CircuitPlayground(port)
 
+
 def accel_data(x, y, z):
+    """Print accelerometer data nicely """
     print('Received accelerometer data!')
     print('X = {0}'.format(x))
     print('Y = {0}'.format(y))
     print('Z = {0}'.format(z))
 
-# Grab an accelerometer reading every 2 seconds.
+
 print('Printing accelerometer data (Ctrl-C to quit)...')
+# Request and read accelerometer data. Sleep for 2 seconds betweem reads."
 while True:
     board.read_accel(accel_data)
     time.sleep(2.0)
 
-# Close Firmata board connection when done.
+# Close Circuit Playground board connection when done.
 board.close()
